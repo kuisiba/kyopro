2c466c524e0139827ea0d56255bff18fd0b39e92
kuisiba <kuisiba@gmail.com>
2019-03-15 22:23:31 +0900

    add chmod

--- gen.sh
+++ gen.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 cp -r template $1
+chmod 744 $1/a/test.sh
+chmod 744 $1/a/open.sh
+chmod 744 $1/b/test.sh
+chmod 744 $1/b/open.sh
+chmod 744 $1/c/test.sh
+chmod 744 $1/c/open.sh
+chmod 744 $1/d/test.sh
+chmod 744 $1/d/open.sh
+chmod 744 $1/e/test.sh
+chmod 744 $1/e/open.sh
+chmod 744 $1/f/test.sh
+chmod 744 $1/f/open.sh
